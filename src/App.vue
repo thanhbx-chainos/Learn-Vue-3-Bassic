6815dd5f959899d293636a24a5801eb54aa6efb5
thanhbx-chainos <thanhbx@chainos.vn>
2022-05-05 18:11:12 +0700

    :tada: Pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main-screen v-if="statusMatch==='default'" @onStart="onHandleBeforeStart($event)"/>
+  <interact-screen v-if="statusMatch==='match'" :cardsContext="settings.cardsContext"/>
+  <coppy-screen/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CoppyrightScreen from '@/components/CoppyrightScreen'
+import MainScreen from "@/components/MainScreen";
+import InteractScreen from "@/components/InteractScreen";
+import {shuffled} from "@/utils/array";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      settings: {
+        totalOfBlock: 0,
+        cardsContext: [],
+        startedAt:null
+      },
+      statusMatch: "default"
+    }
+  },
   components: {
-    HelloWorld
+    CoppyScreen: CoppyrightScreen,
+    MainScreen,
+    InteractScreen
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      this.settings.totalOfBlock = config.totalOfBlock
+
+      const firstCard = Array.from({length: this.settings.totalOfBlock / 2}, (_, i) => i + 1)
+      const secondCard = [...firstCard]
+      const cards = [...firstCard, ...secondCard]
+      this.settings.cardsContext = shuffled(shuffled(shuffled(shuffled(cards))))
+      this.settings.startedAt = new Date().getTime()
+
+      this.statusMatch = 'match'
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import './assets/scss/main.scss';
 </style>
+
